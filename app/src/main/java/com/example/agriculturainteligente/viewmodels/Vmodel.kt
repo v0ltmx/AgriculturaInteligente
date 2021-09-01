13eb1f1d6526d5406390fa1b6aeb27539901cd02
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2021-08-31 21:41:45 -0300

    vmodel

--- app/src/main/java/com/example/agriculturainteligente/viewmodels/Vmodel.kt
+++ app/src/main/java/com/example/agriculturainteligente/viewmodels/Vmodel.kt
@@ -3,11 +3,7 @@ package com.example.agriculturainteligente.viewmodels
 import androidx.lifecycle.ViewModel
 
 class Vmodel: ViewModel() {
-
-
     var resultado1 = "resultado1"
     var resultado2 = "resultado2"
     var resultado3 = "resultado3"
-
-
 }
